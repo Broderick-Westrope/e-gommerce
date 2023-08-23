05f2680310ba9fba5b3555f995cded8e3ae9603a
Broderick Westrope <broderickwestrope@gmail.com>
2023-08-23 11:33:01 +1000

    feat: add golangci-lint installation to dev container post-create script

--- .devcontainer/postCreate.bash
+++ .devcontainer/postCreate.bash
@@ -1,3 +1,7 @@
 #! /bin/bash
 
 go get ./...
+
+# install golangci-lint. binary will be $(go env GOPATH)/bin/golangci-lint
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.54.2
+golangci-lint --version
