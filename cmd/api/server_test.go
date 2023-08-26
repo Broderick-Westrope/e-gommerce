2eb842eb8ad5b1ceef8b2376fec5762e7228defe
Broderick Westrope <broderickwestrope@gmail.com>
2023-08-26 22:51:51 +1000

    test: create a testServer type for easy testing of handlers

---
+++ cmd/api/server_test.go
@@ -0,0 +1,44 @@
+package api
+
+import (
+	"github.com/Broderick-Westrope/e-gommerce/internal/config"
+	"github.com/Broderick-Westrope/e-gommerce/internal/models"
+	"github.com/Broderick-Westrope/e-gommerce/internal/storage"
+	"github.com/go-chi/chi/v5"
+)
+
+type testServer struct {
+	mux     *chi.Mux
+	storage *storage.TestStore
+	logger  config.Logger
+}
+
+func newTestServer() *testServer {
+	return &testServer{
+		mux:     chi.NewRouter(),
+		storage: storage.NewTestStore(),
+		logger:  config.NewLog(),
+	}
+}
+
+func (srv *testServer) Mux() *chi.Mux {
+	return srv.mux
+}
+
+func (srv *testServer) Storage() storage.Storage {
+	return srv.storage
+}
+
+func (srv *testServer) Logger() config.Logger {
+	return srv.logger
+}
+
+func (srv *testServer) MountHandlers() {
+	srv.mux.Route("/v1", func(r chi.Router) {
+		r.Mount("/api/products", ProductRoutes(srv))
+	})
+}
+
+func (srv *testServer) AddTestData(products *[]models.Product) error {
+	return srv.storage.AddProducts(products)
+}
