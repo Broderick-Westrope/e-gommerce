8493c5d08ffceadf9fe69f6d227745ecbfd9ddae
Broderick Westrope <broderickwestrope@gmail.com>
2023-08-26 18:16:34 +1000

    fix: make Product model description nullable

--- internal/models/product.go
+++ internal/models/product.go
@@ -1,11 +1,13 @@
 package models
 
+import "database/sql"
+
 type Product struct {
-	ID            int     `json:"id"`
-	Name          string  `json:"name"`
-	Description   string  `json:"description"`
-	Price         float64 `json:"price"`
-	StockQuantity int     `json:"stock_quantity"`
-	CreatedAt     string  `json:"created_at"`
-	UpdatedAt     string  `json:"updated_at"`
+	ID            int            `json:"id"`
+	Name          string         `json:"name"`
+	Description   sql.NullString `json:"description"`
+	Price         float64        `json:"price"`
+	StockQuantity int            `json:"stock_quantity"`
+	CreatedAt     string         `json:"created_at"`
+	UpdatedAt     string         `json:"updated_at"`
 }
