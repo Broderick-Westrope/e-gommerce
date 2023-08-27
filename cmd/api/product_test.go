ca5fdc0db8fb60b7c153f10c54f60242fdebf188
Broderick Westrope <broderickwestrope@gmail.com>
2023-08-27 22:32:30 +1000

    test: remove TestServer_ProductRoutes; add TestServer_ProductRoutes_GetProducts
    Moving towards having a test function for each handler, but still performing the tests through the exported Server ProductRoutes method

--- cmd/api/product_test.go
+++ cmd/api/product_test.go
@@ -1,58 +1,74 @@
 package api_test
 
 import (
+	"database/sql"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
+
+	"github.com/Broderick-Westrope/e-gommerce/internal/models"
 )
 
-func TestServer_ProductRoutes(t *testing.T) {
+func TestServer_ProductRoutes_GetProducts(t *testing.T) {
+	method := http.MethodGet
+	url := "/v1/api/products"
+
 	srv := newTestServer()
 	srv.MountHandlers()
 
 	t.Run("product routes", func(t *testing.T) {
 		tt := []struct {
 			name               string
-			method             string
-			url                string
+			products           []models.CreateProductRequest
 			expectedStatusCode int
+			expectedProducts   []models.Product
 		}{
 			{
-				"happy path, get products",
-				http.MethodGet,
-				"/v1/api/products",
+				"happy path",
+				[]models.CreateProductRequest{
+					{
+						Name:          "Test Product",
+						Description:   "Test Description",
+						StockQuantity: 10,
+						Price:         1.99,
+					},
+					{
+						Name:          "Test Product 2",
+						Description:   "",
+						StockQuantity: 20,
+						Price:         2.99,
+					},
+				},
 				http.StatusOK,
-			},
-			{
-				"happy path, get product",
-				http.MethodGet,
-				"/v1/api/products/1",
-				http.StatusNotFound,
-			},
-			{
-				"happy path, create product",
-				http.MethodPost,
-				"/v1/api/products/1",
-				http.StatusCreated,
-			},
-			{
-				"happy path, update product",
-				http.MethodPut,
-				"/v1/api/products/1",
-				http.StatusNoContent,
-			},
-			{
-				"happy path, delete product",
-				http.MethodDelete,
-				"/v1/api/products/1",
-				http.StatusNoContent,
+				[]models.Product{
+					{
+						ID:            1,
+						Name:          "Test Product",
+						Description:   sql.NullString{String: "Test Description", Valid: true},
+						StockQuantity: 10,
+						Price:         1.99,
+					},
+					{
+						ID:            2,
+						Name:          "Test Product 2",
+						Description:   sql.NullString{String: "", Valid: false},
+						StockQuantity: 20,
+						Price:         2.99,
+					},
+				},
 			},
 		}
 
 		for _, tc := range tt {
 			t.Run(tc.name, func(t *testing.T) {
+				for _, productReq := range tc.products {
+					srv.Storage().CreateProduct(&productReq)
+				}
+
 				rr := httptest.NewRecorder()
-				req, err := http.NewRequest(tc.method, tc.url, nil)
+				req, err := http.NewRequest(method, url, nil)
 				if err != nil {
 					t.Error(err)
 				}
@@ -61,6 +77,20 @@ func TestServer_ProductRoutes(t *testing.T) {
 
 				if rr.Code != tc.expectedStatusCode {
 					t.Errorf("Status Code: got %d; want %d", rr.Code, tc.expectedStatusCode)
+				}
+
+				products := new([]models.Product)
+				json.NewDecoder(rr.Body).Decode(products)
+
+				if len(*products) != len(tc.expectedProducts) {
+					t.Errorf("Products Length: got %d; want %d", len(*products), len(tc.expectedProducts))
+				}
+				if !reflect.DeepEqual(*products, tc.expectedProducts) {
+					t.Errorf("Products: got %v; want %v", *products, tc.expectedProducts)
+				}
+
+				for _, product := range *products {
+					srv.Storage().DeleteProduct(product.ID)
 				}
 			})
 		}
