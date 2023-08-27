7d67c996ab71d888e133d1d3d8561bd0c76fc155
Broderick Westrope <broderickwestrope@gmail.com>
2023-08-27 21:57:29 +1000

    feat: add CreateProductRequest type
    This type is like Product but does not include the ID and its description is a string (not sql.NullString).

--- internal/models/product.go
+++ internal/models/product.go
@@ -9,3 +9,26 @@ type Product struct {
 	Price         float64        `json:"price"`
 	StockQuantity int            `json:"stock_quantity"`
 }
+
+type CreateProductRequest struct {
+	Name          string  `json:"name"`
+	Description   string  `json:"description"`
+	Price         float64 `json:"price"`
+	StockQuantity int     `json:"stock_quantity"`
+}
+
+func (c *CreateProductRequest) ToProduct(id int) *Product {
+	var isValid bool
+	if c.Description == "" {
+		isValid = false
+	} else {
+		isValid = true
+	}
+	return &Product{
+		ID:            id,
+		Name:          c.Name,
+		Description:   sql.NullString{String: c.Description, Valid: isValid},
+		Price:         c.Price,
+		StockQuantity: c.StockQuantity,
+	}
+}
